62b89bfed73558867d6838a0e20e812c33f45317
Wirardi <109718562+wirardicode@users.noreply.github.com>
2025-07-30 11:04:32 +0700

    Update tunai.js

--- src/tunai.js
+++ src/tunai.js
@@ -50,7 +50,7 @@ function simpan() {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/send", {
+      const response = await fetch("https://b2cc1eb253ef.ngrok-free.app/send", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
